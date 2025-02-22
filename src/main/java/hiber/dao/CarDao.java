32b90e3ad79fdd8023e8cf554723b0248c613710
Imwy345 <kostay2013.18@gmail.com>
2025-02-22 14:55:39 +0300

    2.2.1

--- src/main/java/hiber/dao/CarDao.java
+++
@@ -1,8 +0,0 @@
-package hiber.dao;
-
-import hiber.model.Car;
-
-public interface CarDao {
-    void add(String model,int series);
-    Car getCarById(Long id);
-}
--- src/main/java/hiber/service/CarService.java
+++
@@ -1,8 +0,0 @@
-package hiber.service;
-
-import hiber.model.Car;
-
-public interface CarService {
-    void add(String model,int series);
-    Car getCarById(Long id);
-}
