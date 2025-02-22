37c29cc5edc7221fdcaa6f154bdfdc02ba90f63b
Imwy345 <kostay2013.18@gmail.com>
2025-02-22 15:23:27 +0300

    2.2.3

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -1,19 +1,28 @@
 package web.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.logging.Logger;
+
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    Logger logger = Logger.getLogger(AppInit.class.getName());
 
     // Метод, указывающий на класс конфигурации
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return null;
+        logger.severe("AppInit rootConfigClasses");
+        return new Class<?>[]{
+                HibernateConfig.class};
     }
 
 
     // Добавление конфигурации, в которой инициализируем ViewResolver, для корректного отображения jsp.
     @Override
     protected Class<?>[] getServletConfigClasses() {
+        logger.severe("AppInit servletConfigClasses");
         return new Class<?>[]{
                 WebConfig.class
         };
@@ -23,7 +32,22 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
     /* Данный метод указывает url, на котором будет базироваться приложение */
     @Override
     protected String[] getServletMappings() {
+        logger.severe("AppInit servletMappings");
         return new String[]{"/"};
+    }
+
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        logger.severe("AppInit onStartup");
+        super.onStartup(servletContext);
+        registerHiddenFieldFilter(servletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext servletContext) {
+        logger.severe("AppInit registerHiddenFieldFilter");
+        servletContext.addFilter("hiddenHttpFieldFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
     }
 
 }
--- src/main/java/web/config/WebConfig.java
+++ src/main/java/web/config/WebConfig.java
@@ -11,11 +11,14 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
+import java.util.logging.Logger;
+
 @Configuration
 @EnableWebMvc
-@ComponentScan("web")
+@ComponentScan(basePackages = "web")
 public class WebConfig implements WebMvcConfigurer {
-
+    Logger logger = Logger.getLogger(WebConfig.class.getName());
+    // Настройка Thymeleaf
     private final ApplicationContext applicationContext;
 
     public WebConfig(ApplicationContext applicationContext) {
@@ -25,26 +28,32 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
+        logger.severe("SpringResourceTemplateResolver");
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
         templateResolver.setApplicationContext(applicationContext);
         templateResolver.setPrefix("/WEB-INF/pages/");
         templateResolver.setSuffix(".html");
+        logger.severe("SpringResourceTemplateResolver return");
         return templateResolver;
     }
 
     @Bean
     public SpringTemplateEngine templateEngine() {
+        logger.severe("SpringTemplateEngine");
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(templateResolver());
         templateEngine.setEnableSpringELCompiler(true);
+        logger.severe("SpringTemplateEngine return");
         return templateEngine;
     }
 
 
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
+        logger.severe("ViewResolverRegistry");
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
+        logger.severe("ViewResolver end");
     }
 }
